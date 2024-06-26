e40123f593190a96afaeef92c16258fccd2e3d1a
Luke Cassady-Dorion <luke@SPStories.com>
2024-06-26 10:23:27 +0100

    added diagrams

--- components/ArchitectureBento.tsx
+++ components/ArchitectureBento.tsx
@@ -1,81 +1,69 @@
+import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import HeaderFog from "@/public/assets/use-cases/header-fog.webp";
-import { FaLocationArrow } from "react-icons/fa6";
 
-const bentoData: {
+interface Bento {
 	title: string;
 	description: string;
 	image?: string;
 	href?: string;
-	properties: {
-		className: string;
-	};
-}[] = [
+}
+
+const bentoData: Bento[] = [
 	{
 		title: "Efficient Sampling",
 		description: "Enables economical storage scaling with a 24-hour sampling window for 16TB HDDs.",
-		image: "/diagrams/sampling.png",
-		properties: {
-			className: "col-span-12 md:col-span-8",
-		},
+		image: "/diagrams/efficient-sampling.jpg",
+		href: "/efficiency",
 	},
 	{
 		title: "Fast 2D Packing",
-		href: "/protocol/packing",
-		image: "/diagrams/packing.png",
 		description:
 			"Combined with staked mining addresses, enables incentive-compatible packing that scales horizontally, reduces packing costs, and increases the network's ability to ingest data.",
-		properties: {
-			className: "col-span-12 md:col-span-4",
-		},
+		image: "/diagrams/fast-2d-packing.jpg",
+		href: "/protocol/packing",
 	},
 	{
 		title: "Capacity Mining",
-		href: "/overview/mining",
-		image: "/diagrams/capacity-mining.png",
 		description: "Allows pre-packing of empty capacity partitions for efficient data ingress.",
-		properties: {
-			className: "col-span-12 md:col-span-4",
-		},
+		image: "/diagrams/capacity-mining.jpg",
+		href: "/overview/mining",
 	},
 	{
 		title: "Verifiability",
 		description:
 			"End users can easily verify onchain which mining addresses are storing their data. 2D Unpacking is cross-platform and can be done on consumer hardware, even in a browser.",
 		href: "/verifiability",
-		properties: {
-			className: "col-span-12 md:col-span-8",
-		},
 	},
 ];
 
 const ArchitectureBento: React.FC = () => {
 	return (
-		<div className="overflow-clip mt-5">
-			<section className="container px-5 md:px-0 mx-auto">
-				<div className="grid grid-cols-12 gap-x-4 gap-y-4">
-					{bentoData.map((bento) => (
-						<div
-							key={bento.title}
-							className={`animated-border cursor-pointer ${bento.properties.className} p-4 h-auto rounded-xl flex items-start justify-end flex-col bg-gradient-to-b from-[#171717]/50 to-[#171717]/20`}
-						>
-							{bento.image && (
-								<img src={bento.image} alt={bento.title} className="w-full h-full object-cover rounded-t-lg mb-4" />
-							)}
-							<h3 className="text-lg flex flex-row font-bold tracking-tight text-white">
-								{bento.title}
-								{bento.href && (
-									<Link className="underline ml-5 mt-1" href={bento.href}>
-										<FaLocationArrow className="transform transition-transform duration-300 ease-out hover:scale-105 hover:text-[#FF8451]" />
-									</Link>
-								)}
-							</h3>
-							<p className="text-base font-light text-[#B7B7B7]">{bento.description}</p>
+		<div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-5">
+			{bentoData.map((bento) => (
+				<div
+					key={bento.title}
+					className="border border-gray-700 cursor-pointer p-4 h-auto rounded-xl flex flex-col bg-gradient-to-b from-[#171717]/50 to-[#171717]/20 relative"
+				>
+					{bento.image && (
+						<div className="relative w-full h-60">
+							{" "}
+							{/* Adjust height as necessary */}
+							<Image src={bento.image} alt={bento.title} layout="fill" className="rounded-t-lg p-2" />
 						</div>
-					))}
+					)}
+					<h3 className="text-lg font-bold tracking-tight text-white mt-4">
+						{bento.href ? (
+							<Link href={bento.href} className="underline">
+								{bento.title}
+							</Link>
+						) : (
+							bento.title // Render title without link if `href` is not provided
+						)}
+					</h3>
+					<p className="text-base font-light text-[#B7B7B7]">{bento.description}</p>
 				</div>
-			</section>
+			))}
 		</div>
 	);
 };
--- components/ComponentBento.tsx
+++ components/ComponentBento.tsx
@@ -1,14 +1,16 @@
 import React from "react";
+import Image from "next/image";
 import Link from "next/link";
-import { FaLocationArrow } from "react-icons/fa6";
 
-const componentBentoData: {
+interface ComponentBento {
 	title: string;
 	description: string;
 	details: string[];
 	image?: string;
 	href?: string;
-}[] = [
+}
+
+const componentBentoData: ComponentBento[] = [
 	{
 		title: "Proof of Work + Staking",
 		description:
@@ -18,7 +20,7 @@ const componentBentoData: {
 	{
 		title: "Submit + Publish Ledgers",
 		description: "Allows the protocol to:",
-		image: "/diagrams/ledgers.png",
+		image: "/diagrams/submit-publish-ledgers.jpg",
 		details: [
 			"Make commitments about published data",
 			"Provide reliable access for the execution layer by managing data upload and storage proofs",
@@ -41,41 +43,41 @@ const componentBentoData: {
 
 const ComponentBento: React.FC = () => {
 	return (
-		<section className="container px-5 md:px-0 mx-auto mt-5">
-			<div className="grid grid-cols-12 gap-x-4 gap-y-4">
-				{componentBentoData.map((component) => (
-					<div
-						key={component.title}
-						className="col-span-12 md:col-span-6 animated-border cursor-pointer p-5 h-auto rounded-xl flex items-start justify-end flex-col bg-gradient-to-b from-[#171717]/50 to-[#171717]/20"
-					>
-						{component.image && (
-							<img
+		<div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-5">
+			{componentBentoData.map((component) => (
+				<div
+					key={component.title}
+					className="border border-gray-700 cursor-pointer p-4 h-auto rounded-xl flex flex-col bg-gradient-to-b from-[#171717]/50 to-[#171717]/20 relative"
+				>
+					{component.image && (
+						<div className="relative w-full h-60">
+							{" "}
+							{/* Adjust height as necessary */}
+							<Image
 								src={component.image}
 								alt={component.title}
-								className="w-full h-full object-cover rounded-t-lg mb-4"
+								layout="fill"
+								objectFit="cover"
+								className="rounded-t-lg"
 							/>
-						)}
-						<h3 className="text-lg flex flex-row font-bold tracking-tight text-white">
+						</div>
+					)}
+					<h3 className="text-lg font-bold tracking-tight text-white mt-4">
+						<Link className="underline" href={component.href || "#"}>
 							{component.title}
-
-							{component.href && (
-								<Link className="underline ml-5 mt-1" href={component.href}>
-									<FaLocationArrow className="transform transition-transform duration-300 ease-out hover:scale-105 hover:text-[#FF8451]" />
-								</Link>
-							)}
-						</h3>
-						<p className="text-base font-light text-[#B7B7B7]">{component.description}</p>
-						{component.details.length > 0 && (
-							<ul className="list-disc list-inside text-base font-light text-[#B7B7B7] mt-2">
-								{component.details.map((detail, index) => (
-									<li key={index}>{detail}</li>
-								))}
-							</ul>
-						)}
-					</div>
-				))}
-			</div>
-		</section>
+						</Link>
+					</h3>
+					<p className="text-base font-light text-[#B7B7B7]">{component.description}</p>
+					{component.details.length > 0 && (
+						<ul className="list-disc list-inside text-base font-light text-[#B7B7B7] mt-2">
+							{component.details.map((detail, index) => (
+								<li key={index}>{detail}</li>
+							))}
+						</ul>
+					)}
+				</div>
+			))}
+		</div>
 	);
 };
 
