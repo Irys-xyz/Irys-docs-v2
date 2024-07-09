cf8b55267ccd91503ea8708d61f29bc047e0ce1d
Luke Cassady-Dorion <luke@SPStories.com>
2024-07-09 11:04:57 +0100

    updated rive files

--- components/Irys2dPacking.tsx
+++ components/Irys2dPacking.tsx
@@ -88,56 +88,57 @@ const Irys2dPacking: React.FC = () => {
 					/>
 				)}
 			</div>
+			<div className="px-5 pb-5">
+				<div className="flex flex-row mt-5">
+					<div className="w-20">
+						<button
+							className="px-3 relative flex items-center justify-center bg-[#FF8451] rounded-full text-xl m-2 text-white w-10 h-10 transform transition-transform duration-500 ease-in-out hover:scale-105"
+							onClick={() => setStep(1)}
+						>
+							<CiPlay1 className="absolute text-4xl ml-1" style={{ fill: "white" }} />
+							<span className="absolute text-xs font-bold">1</span>
+						</button>
+					</div>
+					<div className="flex flex-col items-start ml-5">
+						<p className="font-bold">Partitions</p>
+						The foundational building block of storage on Irys is the 16TB partition. Irys partitions data logically
+						into these partitions which can then be stored on individual 16TB HDDs. SSDs do not provide a mining
+						advantage for reasons that will be discussed later.
+					</div>
+				</div>
 
-			<div className="flex flex-row mt-5">
-				<div className="w-20">
-					<button
-						className="px-3 relative flex items-center justify-center bg-[#FF8451] rounded-full text-xl m-2 text-white w-10 h-10 transform transition-transform duration-500 ease-in-out hover:scale-105"
-						onClick={() => setStep(1)}
-					>
-						<CiPlay1 className="absolute text-4xl ml-1" style={{ fill: "white" }} />
-						<span className="absolute text-xs font-bold">1</span>
-					</button>
+				<div className="flex flex-row mt-5">
+					<div className="w-20">
+						<button
+							className="px-3 relative flex items-center justify-center bg-[#FF8451] rounded-full text-xl m-2 text-white w-10 h-10 transform transition-transform duration-500 ease-in-out hover:scale-105"
+							onClick={() => setStep(2)}
+						>
+							<CiPlay1 className="absolute text-4xl ml-1" style={{ fill: "white" }} />
+							<span className="absolute text-xs font-bold">2</span>
+						</button>
+					</div>
+					<div className="flex flex-col items-start ml-5">
+						<p className="font-bold">Chunks</p>
+						Each partition is further divided into 256KiB chunks which are read during the mining process to produce
+						hashes which have the potential to be a mining solution and enable the miner to announce a block to the
+						network and earn rewards.
+					</div>
 				</div>
-				<div className="flex flex-col items-start ml-5">
-					<p className="font-bold">Partitions</p>
-					The foundational building block of storage on Irys is the 16TB partition. Irys partitions data logically into
-					these partitions which can then be stored on individual 16TB HDDs. SSDs do not provide a mining advantage for
-					reasons that will be discussed later.
-				</div>
-			</div>
-
-			<div className="flex flex-row mt-5">
-				<div className="w-20">
-					<button
-						className="px-3 relative flex items-center justify-center bg-[#FF8451] rounded-full text-xl m-2 text-white w-10 h-10 transform transition-transform duration-500 ease-in-out hover:scale-105"
-						onClick={() => setStep(2)}
-					>
-						<CiPlay1 className="absolute text-4xl ml-1" style={{ fill: "white" }} />
-						<span className="absolute text-xs font-bold">2</span>
-					</button>
-				</div>
-				<div className="flex flex-col items-start ml-5">
-					<p className="font-bold">Chunks</p>
-					Each partition is further divided into 256KiB chunks which are read during the mining process to produce
-					hashes which have the potential to be a mining solution and enable the miner to announce a block to the
-					network and earn rewards.
-				</div>
-			</div>
-			<div className="flex flex-row mt-5">
-				<div className="w-20">
-					<button
-						className="px-3 relative flex items-center justify-center bg-[#FF8451] rounded-full text-xl m-2 text-white w-10 h-10 transform transition-transform duration-500 ease-in-out hover:scale-105"
-						onClick={() => setStep(3)}
-					>
-						<CiPlay1 className="absolute text-4xl ml-1" style={{ fill: "white" }} />
-						<span className="absolute text-xs font-bold">3</span>
-					</button>
-				</div>
-				<div className="flex flex-col items-start ml-5">
-					<p className="font-bold">Segments</p>
-					Each chunk is further divided into segments 32 bytes in size. These segments contain the packing entropy that
-					is the result of a SHA-256 hash.
+				<div className="flex flex-row mt-5">
+					<div className="w-20">
+						<button
+							className="px-3 relative flex items-center justify-center bg-[#FF8451] rounded-full text-xl m-2 text-white w-10 h-10 transform transition-transform duration-500 ease-in-out hover:scale-105"
+							onClick={() => setStep(3)}
+						>
+							<CiPlay1 className="absolute text-4xl ml-1" style={{ fill: "white" }} />
+							<span className="absolute text-xs font-bold">3</span>
+						</button>
+					</div>
+					<div className="flex flex-col items-start ml-5">
+						<p className="font-bold">Segments</p>
+						Each chunk is further divided into segments 32 bytes in size. These segments contain the packing entropy
+						that is the result of a SHA-256 hash.
+					</div>
 				</div>
 			</div>
 		</div>
