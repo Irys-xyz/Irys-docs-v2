f959ef206c8a6754802fcf05c1cadf1891416cff
Luke Cassady-Dorion <luke@SPStories.com>
2024-07-24 14:15:56 +0700

    new sidebar, reworked text

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,12 +1,41 @@
-// import Footer from "./components/Footer";
 import IrysLogoText from "./components/svg/IrysLogoText";
 import Footer from "./components/footer/footer";
-import { useConfig } from "nextra-theme-docs";
 import { useRouter } from "next/router";
+import {
+	MdAutoAwesomeMosaic,
+	MdOutlineMonetizationOn,
+	MdOutlineAccountBalance,
+	MdOutlineMining,
+	MdOutlineDescription,
+	MdOutlineVerified,
+	MdOutlineStorage,
+	MdOutlineMenuBook,
+	MdOutlineViewList,
+	MdOutlineSwapHoriz,
+	MdOutlineViewQuilt,
+	Md360,
+	MdBlurLinear,
+} from "react-icons/md";
 
 const FooterWrapper = () => {
 	const router = useRouter();
 	return <Footer />;
+};
+
+// Create a mapping for the sub-items (using a single icon for now)
+const iconMapping = {
+	architecture: <MdAutoAwesomeMosaic />,
+	pricing: <MdOutlineMonetizationOn />,
+	treasury: <MdOutlineAccountBalance />,
+	mining: <MdBlurLinear />,
+	overview: <Md360 />,
+	"use-cases": <MdOutlineDescription />,
+	verifiability: <MdOutlineVerified />,
+	"provable-storage": <MdOutlineStorage />,
+	ledgers: <MdOutlineMenuBook />,
+	partitions: <MdOutlineViewList />,
+	transactions: <MdOutlineSwapHoriz />,
+	"2d-packing": <MdOutlineViewQuilt />,
 };
 
 export default {
@@ -17,6 +46,7 @@ export default {
 		useOptions() {
 			return {
 				dark: "Dark",
+				light: "Light",
 			};
 		},
 	},
@@ -28,6 +58,27 @@ export default {
 	},
 	sidebar: {
 		toggleButton: true,
+		titleComponent({ title, type }) {
+			const iconStyles = { fontSize: "1.1rem", marginRight: "0.5rem" };
+			const containerStyles = { display: "flex", alignItems: "center" };
+
+			if (type !== "separator") {
+				const key = title.toLowerCase().replace(/\s+/g, "-");
+				console.log(key);
+				const icon = iconMapping[key];
+				return (
+					<div style={containerStyles}>
+						{icon && <span style={iconStyles}>{icon}</span>}
+						<span>{title}</span>
+					</div>
+				);
+			}
+			return (
+				<div style={containerStyles}>
+					<span>{title}</span>
+				</div>
+			);
+		},
 	},
 	logo: <IrysLogoText width={65} />,
 	project: {
@@ -59,11 +110,11 @@ export default {
 				<meta name="twitter:card" content="summary_large_image" />
 				<script type="application/ld+json">
 					{`{
-  "@context" : "https://schema.org",
-  "@type" : "WebSite",
-  "name" : "Irys",
-  "url" : "https://docs.irys.xyz"
-}`}
+            "@context" : "https://schema.org",
+            "@type" : "WebSite",
+            "name" : "Irys",
+            "url" : "https://docs.irys.xyz"
+          }`}
 				</script>
 			</>
 		);
