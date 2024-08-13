3ab81214198e838dc2499e2744c36379aef549f7
Luke Cassady-Dorion <luke@SPStories.com>
2024-08-13 17:51:54 +0700

    Changed currency to token

--- components/IrysRestApi/CustomLayoutPlugin.tsx
+++ components/IrysRestApi/CustomLayoutPlugin.tsx
@@ -20,12 +20,37 @@ class CustomLayout extends React.Component<CustomLayoutProps> {
   }
 }
 
+// Plugin to disable "Try it out" button for specific endpoints
+const DisableTryItOutPlugin = () => {
+  console.log("DisableTryItOutPlugin=");
+
+  return {
+    wrapComponents: {
+      Operation: (Original: React.ComponentType<any>, { React }: any) => (props: any) => {
+        const isDisabled = [
+          "/account/balance/{currency}",
+          "/account/approval"
+          // Paths for which to hide the Try it out button
+        ].includes(props.operationProps.path)
+        console.log("DisableTryItOutPlugin=",props.operationProps.path);
+        console.log({isDisabled})
+        if (isDisabled) { 
+          return <div>{props.operationProps.operation.operationId} (Try It Out disabled)</div>
+        }
+
+        return <Original {...props} />
+      },
+    },
+  }
+}
+
 // Create the plugin that provides our layout component
 const CustomLayoutPlugin = () => {
   return {
     components: {
       CustomLayout: CustomLayout,
     },
+    plugins: [DisableTryItOutPlugin] 
   }
 }
 
