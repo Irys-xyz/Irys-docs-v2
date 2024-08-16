078cab36ff29f2ba4180180d0cd6046431ef7e2b
TheHoltz <holtzs.william@gmail.com>
2024-08-15 21:48:26 -0300

    feat: :sparkles: navbar, background

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -7,97 +7,99 @@ import SOCIAL_MEDIAS_FOOTER from "./footer-socials";
 type Props = {};
 
 const Footer = (props: Props) => {
-	const OPTIONS: {
-		title: string;
-		items: {
-			title: string;
-			href: string;
-		}[];
-	}[] = [
-		{
-			title: "OVERVIEW",
-			items: [
-				{
-					title: "What is Irys?",
-					href: "/learn-about-irys/what-is-irys",
-				},
-				{
-					title: "What is A Datachain?",
-					href: "/learn-about-irys/what-is-datachain",
-				},
-				{
-					title: "Build With Us!",
-					href: "/build-with-us",
-				},
-			],
-		},
-		{
-			title: "TOOLS FOR ARWEAVE",
-			items: [
-				{
-					title: "SDK",
-					href: "https://arweave-tools.irys.xyz/irys-sdk",
-				},
-				{
-					title: "Query Package",
-					href: "https://arweave-tools.irys.xyz/querying/query-package",
-				},
-				{
-					title: "CLI",
-					href: "https://arweave-tools.irys.xyz/cli",
-				},
-			],
-		},
-	];
+  const OPTIONS: {
+    title: string;
+    items: {
+      title: string;
+      href: string;
+    }[];
+  }[] = [
+    {
+      title: "OVERVIEW",
+      items: [
+        {
+          title: "What is Irys?",
+          href: "/learn-about-irys/what-is-irys",
+        },
+        {
+          title: "What is A Datachain?",
+          href: "/learn-about-irys/what-is-datachain",
+        },
+        {
+          title: "Build With Us!",
+          href: "/build-with-us",
+        },
+      ],
+    },
+    {
+      title: "TOOLS FOR ARWEAVE",
+      items: [
+        {
+          title: "SDK",
+          href: "https://arweave-tools.irys.xyz/irys-sdk",
+        },
+        {
+          title: "Query Package",
+          href: "https://arweave-tools.irys.xyz/querying/query-package",
+        },
+        {
+          title: "CLI",
+          href: "https://arweave-tools.irys.xyz/cli",
+        },
+      ],
+    },
+  ];
 
-	return (
-		<footer className="max-w-[1392px] flex flex-col gap-28 my-20 px-7 md:px-0 mx-auto pb-10 -mb-10">
-			<div className="grid grid-cols-12 text-white light gap-y-20">
-				<div className="md:col-span-4 lg:col-span-4 col-span-12 ml-5">
-					<Image src={SvgIrysLogo} alt="Irys Logo" width={99} />
-				</div>
-				{OPTIONS.map((option) => (
-					<div
-						key={option.title}
-						className="col-span-6 md:col-span-4 lg:col-span-4 text-base flex flex-col gap-6"
-					>
-						<div className="font-bold">{option.title}</div>
-						<ul className="font-light text-[#B7B7B7] leading-4 flex flex-col gap-5">
-							{option.items.map((item) => (
-								<Link href={item.href} key={item.href}>
-									<li className="hover:underline">{item.title}</li>
-								</Link>
-							))}
-						</ul>
-					</div>
-				))}
-			</div>
+  return (
+    <footer className="max-w-[1392px] flex flex-col gap-28 my-20 px-7 md:px-0 mx-auto pb-10 -mb-10">
+      <div className="grid grid-cols-12 text-white light gap-y-20">
+        <div className="md:col-span-4 lg:col-span-4 col-span-12 ml-5">
+          <Image src={SvgIrysLogo} alt="Irys Logo" width={99} />
+        </div>
+        {OPTIONS.map((option) => (
+          <div
+            key={option.title}
+            className="col-span-6 md:col-span-4 lg:col-span-4 text-base flex flex-col gap-6"
+          >
+            <div className="font-bold">{option.title}</div>
+            <ul className="font-light text-[#B7B7B7] leading-4 flex flex-col gap-5">
+              {option.items.map((item) => (
+                <Link href={item.href} key={item.href}>
+                  <li className="hover:underline">{item.title}</li>
+                </Link>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
 
-			<div className="flex flex-col gap-8 md:gap-14">
-				<div className="bg-[#0B0B0B] grid grid-cols-12 rounded-xl">
-					<div className="col-span-12 md:col-span-6 flex gap-4 flex-col p-7 md:p-12">
-						<h2 className="text-white text-3xl font-bold">Join the Irys Community</h2>
-					</div>
-					<div className="col-span-12 md:col-span-6 gap-2 flex items-center justify-center p-12">
-						{SOCIAL_MEDIAS_FOOTER.map((social, index) => (
-							<a
-								key={index}
-								href={social.link}
-								target="_blank"
-								rel="noopener noreferrer"
-								className="relative isolated cursor-pointer col-span-12 md:col-span-4 w-14 h-14 rounded-xl flex items-center justify-center bg-gradient-to-b from-[#171717] to-[#08080833]"
-							>
-								<social.icon />
-							</a>
-						))}
-					</div>
-				</div>
-				<div className="text-[#B7B7B7] text-center md:text-start">
-					{new Date().getFullYear()} Irys. All rights reserved.
-				</div>
-			</div>
-		</footer>
-	);
+      <div className="flex flex-col gap-8 md:gap-14">
+        <div className="bg-[#0B0B0B] grid grid-cols-12 rounded-xl">
+          <div className="col-span-12 md:col-span-6 flex gap-4 flex-col p-7 md:p-12">
+            <h2 className="text-white text-3xl font-bold">
+              Join the Irys Community
+            </h2>
+          </div>
+          <div className="col-span-12 md:col-span-6 gap-2 flex items-center justify-center p-12">
+            {SOCIAL_MEDIAS_FOOTER.map((social, index) => (
+              <a
+                key={index}
+                href={social.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="relative isolated cursor-pointer col-span-12 md:col-span-4 w-14 h-14 rounded-xl flex items-center justify-center bg-gradient-to-b from-[#171717] to-[#08080833]"
+              >
+                <social.icon />
+              </a>
+            ))}
+          </div>
+        </div>
+        <div className="text-[#B7B7B7] text-center md:text-start">
+          {new Date().getFullYear()} Irys. All rights reserved.
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- theme.config.jsx
+++ theme.config.jsx
@@ -1,143 +1,150 @@
 import IrysLogoText from "./components/svg/IrysLogoText";
+import NavOptions from "./components/nav-options/nav-options";
 import Footer from "./components/footer/footer";
 import { useRouter } from "next/router";
 import {
-	MdOutlineAccountBalance,
-	MdOutlineDescription,
-	MdOutlineVerified,
-	MdOutlineStorage,
-	MdOutlineMenuBook,
-	MdOutlineViewList,
-	MdOutlineSwapHoriz,
-	MdOutlineViewQuilt,
-	MdBlurLinear,
-	MdFlipToBack,
-	MdDataObject,
-	MdDataset,
-	MdOutlineCropFree,
-	MdOutlineQuestionMark,
-	MdOutlineSync,
-	MdOutlineBlurOn,
-	MdOutlineAccountTree,
-	MdOutlineBolt,
-	MdOutlineBubbleChart,
-	MdOutlineBuild,
+  MdOutlineAccountBalance,
+  MdOutlineDescription,
+  MdOutlineVerified,
+  MdOutlineStorage,
+  MdOutlineMenuBook,
+  MdOutlineViewList,
+  MdOutlineSwapHoriz,
+  MdOutlineViewQuilt,
+  MdBlurLinear,
+  MdFlipToBack,
+  MdDataObject,
+  MdDataset,
+  MdOutlineCropFree,
+  MdOutlineQuestionMark,
+  MdOutlineSync,
+  MdOutlineBlurOn,
+  MdOutlineAccountTree,
+  MdOutlineBolt,
+  MdOutlineBubbleChart,
+  MdOutlineBuild,
 } from "react-icons/md";
 
+import IrysInteractiveEye from "./components/irys-logo/irys-logo";
+
 const FooterWrapper = () => {
-	const router = useRouter();
-	return (
-		<div className="border-t border-[#222222] mt-10 mx-auto bg-[#111111]">
-			{" "}
-			<Footer />
-		</div>
-	);
+  const router = useRouter();
+  return (
+    <div className="border-t border-[#222222] mt-10 mx-auto bg-[#111111]">
+      {" "}
+      <Footer />
+    </div>
+  );
 };
 
 // Create a mapping for the sub-items
 const iconMapping = {
-	"What-is-Irys?": <MdOutlineQuestionMark color="#ff8451" />,
-	"What-is-a-Datachain?": <MdDataset color="#ff8451" />,
-	"Why-Irys-Exists": <MdOutlineCropFree color="#ff8451" />,
-	"How-Irys-Solves-These-Problems": <MdOutlineBuild color="#ff8451" />,
+  "What-is-Irys?": <MdOutlineQuestionMark color="#ff8451" />,
+  "What-is-a-Datachain?": <MdDataset color="#ff8451" />,
+  "Why-Irys-Exists": <MdOutlineCropFree color="#ff8451" />,
+  "How-Irys-Solves-These-Problems": <MdOutlineBuild color="#ff8451" />,
 
-	"Programmable-Data": <MdDataObject color="#ff8451" />,
-	Verifiability: <MdOutlineVerified color="#ff8451" />,
-	IrysVM: <MdOutlineBubbleChart color="#ff8451" />,
+  "Programmable-Data": <MdDataObject color="#ff8451" />,
+  Verifiability: <MdOutlineVerified color="#ff8451" />,
+  IrysVM: <MdOutlineBubbleChart color="#ff8451" />,
 
-	"Running-a-Node": <MdOutlineAccountTree color="#ff8451" />,
-	"2D-Packing": <MdOutlineViewQuilt color="#ff8451" />,
-	"Efficient-Sampling": <MdFlipToBack color="#ff8451" />,
+  "Running-a-Node": <MdOutlineAccountTree color="#ff8451" />,
+  "2D-Packing": <MdOutlineViewQuilt color="#ff8451" />,
+  "Efficient-Sampling": <MdFlipToBack color="#ff8451" />,
 
-	"Protocol-Overview": <MdOutlineStorage color="#ff8451" />,
-	Partitions: <MdOutlineViewList color="#ff8451" />,
-	Ledgers: <MdOutlineMenuBook color="#ff8451" />,
-	Transactions: <MdOutlineSwapHoriz color="#ff8451" />,
-	"Proof-of-Work-+-Staking": <MdOutlineBlurOn color="#ff8451" />,
-	"Fast-Finality": <MdOutlineBolt color="#ff8451" />,
+  "Protocol-Overview": <MdOutlineStorage color="#ff8451" />,
+  Partitions: <MdOutlineViewList color="#ff8451" />,
+  Ledgers: <MdOutlineMenuBook color="#ff8451" />,
+  Transactions: <MdOutlineSwapHoriz color="#ff8451" />,
+  "Proof-of-Work-+-Staking": <MdOutlineBlurOn color="#ff8451" />,
+  "Fast-Finality": <MdOutlineBolt color="#ff8451" />,
 };
 
 export default {
-	nextThemes: {
-		defaultTheme: "system",
-	},
-	themeSwitch: {
-		useOptions() {
-			return {
-				dark: "Dark",
-				light: "Light",
-				system: "System",
-			};
-		},
-	},
-	editLink: {
-		component: () => null,
-	},
-	feedback: {
-		content: () => null,
-	},
-	sidebar: {
-		toggleButton: true,
-		defaultMenuCollapseLevel: 2,
-		titleComponent({ title, type }) {
-			const iconStyles = { fontSize: "1.1rem", marginRight: "0.5rem" };
-			const containerStyles = { display: "flex", alignItems: "center" };
+  nextThemes: {
+    defaultTheme: "system",
+  },
+  themeSwitch: {
+    useOptions() {
+      return {
+        dark: "Dark",
+        light: "Light",
+        system: "System",
+      };
+    },
+  },
+  editLink: {
+    component: () => null,
+  },
+  feedback: {
+    content: () => null,
+  },
+  sidebar: {
+    toggleButton: true,
+    defaultMenuCollapseLevel: 2,
+    titleComponent({ title, type }) {
+      const iconStyles = { fontSize: "1.1rem", marginRight: "0.5rem" };
+      const containerStyles = { display: "flex", alignItems: "center" };
 
-			if (type !== "separator") {
-				const key = title.replace(/\s+/g, "-");
-				const icon = iconMapping[key];
-				return (
-					<div style={containerStyles}>
-						{icon && <span style={iconStyles}>{icon}</span>}
-						<span>{title}</span>
-					</div>
-				);
-			}
+      if (type !== "separator") {
+        const key = title.replace(/\s+/g, "-");
+        const icon = iconMapping[key];
+        return (
+          <div style={containerStyles}>
+            {icon && <span style={iconStyles}>{icon}</span>}
+            <span>{title}</span>
+          </div>
+        );
+      }
 
-			return (
-				<div style={containerStyles}>
-					<span>{title}</span>
-				</div>
-			);
-		},
-	},
-	logo: <IrysLogoText width={65} />,
-	project: {
-		link: "https://github.com/Irys-xyz/",
-	},
-	footer: {
-		component: FooterWrapper,
-	},
-	banner: {},
-	useNextSeoProps() {
-		const { asPath } = useRouter();
-		if (asPath !== "/") {
-			return {
-				titleTemplate: "Irys | %s",
-			};
-		} else {
-			return {
-				titleTemplate: "Irys | Docs",
-			};
-		}
-	},
-	head: () => {
-		return (
-			<>
-				<meta property="og:site_name" content="Irys" />
-				<meta property="og:url" content="https://docs.irys.xyz" />
-				<meta property="og:type" content="website" />
-				<meta property="og:image" content="/irys-open-graph.jpg" />
-				<meta name="twitter:card" content="summary_large_image" />
-				<script type="application/ld+json">
-					{`{
+      return (
+        <div style={containerStyles}>
+          <span>{title}</span>
+        </div>
+      );
+    },
+  },
+  // logo: <IrysLogoText width={65} />,
+  logo: <IrysInteractiveEye />,
+  // project: {
+  //   link: "https://github.com/Irys-xyz/",
+  // },
+  footer: {
+    component: FooterWrapper,
+  },
+  banner: {},
+  useNextSeoProps() {
+    const { asPath } = useRouter();
+    if (asPath !== "/") {
+      return {
+        titleTemplate: "Irys | %s",
+      };
+    } else {
+      return {
+        titleTemplate: "Irys | Docs",
+      };
+    }
+  },
+  navbar: {
+    extraContent: () => <NavOptions />
+  },
+  head: () => {
+    return (
+      <>
+        <meta property="og:site_name" content="Irys" />
+        <meta property="og:url" content="https://docs.irys.xyz" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="/irys-open-graph.jpg" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <script type="application/ld+json">
+          {`{
             "@context" : "https://schema.org",
             "@type" : "WebSite",
             "name" : "Irys",
             "url" : "https://docs.irys.xyz"
           }`}
-				</script>
-			</>
-		);
-	},
+        </script>
+      </>
+    );
+  },
 };
