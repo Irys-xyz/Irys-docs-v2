7222ea71250a354f1ee9cbcd4e843ed1e839cacd
Luke Cassady-Dorion <luke@SPStories.com>
2024-08-30 17:07:48 +0700

    removed landing page

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,19 @@ import nextra from "nextra";
 const withNextra = nextra({
 	theme: "nextra-theme-docs",
 	themeConfig: "./theme.config.jsx",
-	defaultShowCopyCode: true,
 });
 
-const nextConfig = withNextra();
+const nextConfig = withNextra({
+	async redirects() {
+		return [
+			{
+				source: "/",
+				destination: "/learn/learn-about-irys/what-is-irys",
+				permanent: true,
+			},
+		];
+	},
+});
 
 // If you have other Next.js configurations, you can pass them as the parameter:
 // const nextConfig = withNextra({ /* other next.js config */ });
