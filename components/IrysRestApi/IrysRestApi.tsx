16b8f993cbf1fc0cca7163c3d4a2805812c41fee
Luke Cassady-Dorion <luke@SPStories.com>
2024-09-02 17:51:40 +0700

    updated restapi display bug

--- components/IrysRestApi/IrysRestApi.tsx
+++ components/IrysRestApi/IrysRestApi.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import SwaggerUI, { SwaggerUIProps } from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
 import { CustomLayoutPlugin } from "./CustomLayoutPlugin";
+import openApiSpec from "./irys-open-api.json"; 
 
 interface IrysRestApiProps {
   tags: string[];
@@ -15,7 +16,6 @@ const IrysRestApi: React.FC<IrysRestApiProps> = ({ tags }) => {
       "/chunks/{token}/{txid}/{offset}",
     ];
 
-    // Function to disable "Try it out" buttons
     const disableTryItOutButtons = () => {
       const opblockElements = document.querySelectorAll(
         ".opblock-summary-path[data-path]"
@@ -33,9 +33,7 @@ const IrysRestApi: React.FC<IrysRestApiProps> = ({ tags }) => {
       });
     };
 
-    // Function to find and log the "token" parameter field
     const logTokenParameterField = () => {
-      // Select the div with the "parameter__name" class that contains "token"
       const tokenFields = document.querySelectorAll(
         ".parameter__name.required"
       );
@@ -44,37 +42,33 @@ const IrysRestApi: React.FC<IrysRestApiProps> = ({ tags }) => {
         if (element.textContent?.trim() === "token *") {
           console.log("Token field found:", element.textContent?.trim());
           const linkElement = document.createElement("a");
-          linkElement.href = "/build/d/features/supported-tokens"; // Constant for the target URL
+          linkElement.href = "/build/d/features/supported-tokens";
           linkElement.textContent = "token";
-          linkElement.target = "_blank"; // Opens link in a new tab
+          linkElement.target = "_blank";
           linkElement.classList.add("underline");
 
-          // Replace the original text content with the link
-          element.textContent = ""; // Clear existing text content
-          element.appendChild(linkElement); // Append the link
+          element.textContent = "";
+          element.appendChild(linkElement);
         }
       });
     };
 
-    // Create a MutationObserver to watch for changes in the Swagger UI DOM
     const observer = new MutationObserver(() => {
       disableTryItOutButtons();
       logTokenParameterField();
     });
 
-    // Observe changes in the entire document body for any child addition or modification
     observer.observe(document.body, {
       childList: true,
       subtree: true,
     });
 
-    // Initial calls to disable buttons and log the token input on the first render
     disableTryItOutButtons();
     logTokenParameterField();
 
     // Cleanup observer on component unmount
     return () => observer.disconnect();
-  }, []); // Empty dependency array
+  }, []);
 
   const customConfig: SwaggerUIProps = {
     docExpansion: "list",
@@ -84,7 +78,7 @@ const IrysRestApi: React.FC<IrysRestApiProps> = ({ tags }) => {
     layout: "CustomLayout",
     plugins: [CustomLayoutPlugin],
     filter: tags.join(","),
-    url: "/restapi/irys-open-api.json",
+    spec: openApiSpec, 
   };
 
   return (
